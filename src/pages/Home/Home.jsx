8839afcdb879e2f3d8fecde1b8fc41a56abd9a65
PieterJanDelie <pieterjan.delie@gmail.com>
2025-02-04 15:22:25 +0100

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultLayout from "../../layouts/DefaultLayout";
+import "./Home.css";
+
+const Home = () => {
+
+  return (
+    <DefaultLayout>
+      <p>Home</p>
+    </DefaultLayout>
+  );
+};
+
+export default Home;
