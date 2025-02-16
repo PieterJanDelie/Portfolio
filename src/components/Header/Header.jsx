1e46a5e9eed3c17d8d6b059cc0d05fdb79bebbd9
PieterJanDelie <pieterjan.delie@gmail.com>
2025-02-17 00:35:48 +0100

    Fix navbar on phone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './Header.css';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import "./Header.css";
 
 const Header = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav className="navbar">
-      <ul className="navbar-menu">
-        <li className={`navbar-item ${currentPath === '/' ? 'active' : ''}`}>
+      <MenuIcon className="menu-icon" onClick={toggleMenu} />
+      <ul className={`navbar-menu ${menuOpen ? "open" : ""}`}>
+        <li className={`navbar-item ${currentPath === "/" ? "active" : ""}`}>
           <Link to="/">Home</Link>
         </li>
-        <li className={`navbar-item ${currentPath === '/project' ? 'active' : ''}`}>
+        <li
+          className={`navbar-item ${
+            currentPath === "/project" ? "active" : ""
+          }`}
+        >
           <Link to="/project">Project</Link>
         </li>
-        <li className={`navbar-item ${currentPath === '/aboutme' ? 'active' : ''}`}>
+        <li
+          className={`navbar-item ${
+            currentPath === "/aboutme" ? "active" : ""
+          }`}
+        >
           <Link to="/aboutme">About Me</Link>
         </li>
-        <li className={`navbar-item ${currentPath === '/skillscertification' ? 'active' : ''}`}>
+        <li
+          className={`navbar-item ${
+            currentPath === "/skillscertification" ? "active" : ""
+          }`}
+        >
           <Link to="/skillscertification">Skills & Certification</Link>
         </li>
-        <li className={`navbar-item ${currentPath === '/contact' ? 'active' : ''}`}>
+        <li
+          className={`navbar-item ${
+            currentPath === "/contact" ? "active" : ""
+          }`}
+        >
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
