2d6250d1e2af565204fb28fb54a9ba73b97d0a12
PieterJanDelie <pieterjan.delie@student.vives.be>
2025-05-22 09:29:46 +0200

    BiometrIQ raport

--- src/components/Dropdownmenu/Dropdownmenu.jsx
+++ src/components/Dropdownmenu/Dropdownmenu.jsx
@@ -3,16 +3,36 @@ import "./Dropdownmenu.css";
 
 const DropdownMenu = () => {
   return (
-  <div className="dropdown">
-    <button className="dropbtn">Download CV</button>
-    <div className="dropdown-content">
-      {["CV.pdf", "CV_Creative.pdf", "LinkedIn.pdf"].map((file) => (
-        <a key={file} href={require(`../../assets/Docs/CV/${file}`)} target="_blank" rel="noopener noreferrer">
-          {file.replace(".pdf", "").replace(/_/g, " ")}
+    <div className="dropdown">
+      <button className="dropbtn">Download CV</button>
+      <div>
+        <a
+          href={require(`../../assets/Docs/CV/BiometrIQ-Demoraport.pdf`)}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="raport"
+        >
+          raport
         </a>
-      ))}
+      </div>
+      <div className="dropdown-content">
+        {[
+          "CV.pdf",
+          "CV_Creative.pdf",
+          "LinkedIn.pdf",
+        ].map((file) => (
+          <a
+            key={file}
+            href={require(`../../assets/Docs/CV/${file}`)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {file.replace(".pdf", "").replace(/_/g, " ")}
+          </a>
+        ))}
+      </div>
     </div>
-  </div>)
+  );
 };
 
 export default DropdownMenu;
